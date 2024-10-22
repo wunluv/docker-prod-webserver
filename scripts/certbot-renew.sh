5e3146a08d3ae1f2c5ea2da03dc8990141efdf3b
wunluv <santhan@mojahmedia.net>
2024-10-22 13:29:09 +0200

    correcting paths

--- scripts/certbot-renew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script renews certbot certificates and reloads Nginx periodically
-while :; do
-    certbot renew --nginx
-    sleep 12h
-done
--- scripts/reload-nginx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Reload Nginx to apply new certificates
-nginx -s reload
