6b6f749f79dde7c9cac3093e2634730b327d9546
wunluv <santhan@mojahmedia.net>
2024-11-03 18:49:23 +0200

    adding bash script for putting both node apps offline

--- apps/sitedown.sh
+++ apps/sitedown.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Stop Astro container
-docker-compose -f docker-compose-astro.yml down
-sleep 10
-# Stop the Ghost Container
-docker-compose -f docker-compose-ghost.yml down
+# Stop Astro & Ghost container
+docker-compose -f docker-compose-astro.yml -f docker-compose-ghost.yml down
 sleep 10
 # Remove nginx config
 rm /var/www/private/nginx/conf.d/ghost.mojah2.mojahmedia.conf
--- apps/siteup.sh
+++ apps/siteup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Start the Ghost Container
-docker-compose -f docker-compose-ghost.yml up -d
-sleep 10
-docker-compose -f docker-compose-astro.yml up -d
+# Start the Ghost & Astro Container
+docker-compose -f docker-compose-ghost.yml -f docker-compose-astro.yml up -d
 sleep 10
 cp ../nginx/ghost.mojah2.mojahmedia.conf /var/www/private/nginx/conf.d
 cp ../nginx/one.mojah2.mojahmedia.conf /var/www/private/nginx/conf.d
